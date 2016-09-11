4922a6c382b924aa5aa8f82e22503d7cc12a4c52
Dan Corman <dcorman@smarterhq.com>
2016-09-11 13:26:05 -0500

    Added redux thunk and sample wiki api call

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -1,14 +1,13 @@
 import { LOCATION } from '../actions/location';
 
 export default function location(state = "No Location Yet", action) {
-  console.log('HERE', action);
   switch (action.type) {
     case LOCATION:
       var s =  navigator.geolocation.getCurrentPosition(
         (position) => {
           var location = JSON.stringify(position);
           location = JSON.parse(location);
-
+          fetch(`https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&format=json&gscoord=${location.coords.latitude}|${location.coords.longitude}`).then((x) => x.json()).then(x => console.log(x.query.geosearch));
           var a = fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.coords.latitude},${location.coords.longitude} &key=[apikey]`, {method: "GET"})
             .then((response) => response.json()).then(x => {
             location = x.results[3].formatted_address;
